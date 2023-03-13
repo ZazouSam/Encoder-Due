ab75800ffbbb07ae6a0fb8ea2581a11e3b3cd3f9
ZazouSam <zazou2000@hotmail.ca>
2023-03-13 13:54:16 -0400

    format change

--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,19 @@ long oldPosition = -999;
 // Store the count value
 int count = 0;
 
-void btnInterrupt() {
+void btnInterrupt()
+{
   // Print a message when the button is pressed
   Serial.println("Button pressed");
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   Serial.println("Encoder Test");
 
   // Set the initial position of the encoder as the old position
   oldPosition = myEnc.read() / 4;
-
   // Configure pin 27 as input with a pull-up resistor
   pinMode(27, INPUT_PULLUP);
 
@@ -28,20 +29,25 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(27), btnInterrupt, FALLING);
 }
 
-void loop() {
+void loop()
+{
   // Read the current position of the encoder
   long newPosition = myEnc.read() / 4;
 
   // Only update if the position has changed
-  if (newPosition != oldPosition) {
+  if (newPosition != oldPosition)
+  {
 
     // Check the direction of rotation
-    if (newPosition > oldPosition) {
+    if (newPosition > oldPosition)
+    {
       count++;
       Serial.print("Count: ");
       Serial.println(count);
       oldPosition = newPosition;
-    } else if (newPosition < oldPosition) {
+    }
+    else if (newPosition < oldPosition)
+    {
       count--;
       Serial.print("Count: ");
       Serial.println(count);
@@ -49,5 +55,3 @@ void loop() {
     }
   }
 }
-
-
